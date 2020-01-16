cb9d287a69a19a35b7dc15bf726981a815156b84
Andrews Samuel <andrewssamuel@Andrewss-MacBook-Pro.local>
2020-01-15 22:37:37 -0800

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const port = 3000
+const port = 7000
+
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use(bodyParser.json())
 app.use(
@@ -16,11 +36,15 @@ app.get('/', (request, response) => {
 })
 
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+
+app.get('/salesbystore/:id', db.getSalesByStoreId)
+app.get('/salesbycountries', db.getSalesByCountries)
+app.get('/salesbyproductcategories', db.getFSalesByProductCategories)
+app.get('/salesreport', db.getsalesreport)
+app.get('/salesbystore', db.getsalesbystore)
+//app.get('/salesactualbyproductcategories', db.getASalesByProductCategories)
+//app.get('/salesbyproductcategories', db.getFSalesByProductCategories)
+
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
--- queries.js
+++ queries.js
@@ -1,14 +1,18 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api',
-  password: 'password',
+  user: 'postgres',
+  host: 'database-2.csqtwfwchjab.us-west-2.rds.amazonaws.com',
+  database: 'postgres',
+  password: '$ustglobal',
   port: 5432,
 })
 
-const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+
+
+const getSalesByStoreId = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('(SELECT * FROM f_sales_by_store WHERE store_id = $1) union (SELECT * FROM a_sales_by_store WHERE store_id = $1 and year=2017) order by month', [id], (error, results) => {
     if (error) {
       throw error
     }
@@ -16,10 +20,10 @@ const getUsers = (request, response) => {
   })
 }
 
-const getUserById = (request, response) => {
+const getSalesByCountries = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+  pool.query('select a.country_id,a.country_name,a.latitude,a.longitude,round((fsales*tot_per)/1000000,2) as sales_items from  (select sum(sales_items) sales,c.country_name, sp.country_id,c.latitude,c.longitude,(sum(sales_items) / SUM(sum(sales_items)) OVER ()) AS "tot_per"  from a_sales_by_product_03 sp,countries c where sp.country_id = c.country_id and year = 2019 group by c.country_name, sp.country_id, c.latitude, c.longitude) a,(select sum(sales_items) fsales from sales_insights) b',(error, results) => {
     if (error) {
       throw error
     }
@@ -27,48 +31,46 @@ const getUserById = (request, response) => {
   })
 }
 
-const createUser = (request, response) => {
-  const { name, email } = request.body
 
-  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+
+const getFSalesByProductCategories = (request, response) => {
+  //const id = parseInt(request.params.id)
+  str_query = 'select a.product_category_name,a.sales targeted_sales,b.sales actual_sales from (select product_category_name,sum(sales_items) as sales from sales_insights si, product_master pm, product_categories pc where si.product_id = pm.product_id and pm.product_category_id = pc.product_category_id group by product_category_name) as a,(select product_category_name,sum(sales_items) as sales from a_sales_by_product sp, product_master pm, product_categories pc where sp.product_id = pm.product_id and pm.product_category_id = pc.product_category_id and sp.year=2019 group by product_category_name) as b where a.product_category_name = b.product_category_name'
+  pool.query(str_query,(error, results) => {
     if (error) {
       throw error
     }
-    response.status(201).send(`User added with ID: ${results.insertId}`)
+    response.status(200).json(results.rows)
   })
 }
 
-const updateUser = (request, response) => {
-  const id = parseInt(request.params.id)
-  const { name, email } = request.body
-
-  pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
-    (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`User modified with ID: ${id}`)
-    }
-  )
-}
-
-const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id)
-
-  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+const getsalesreport = (request, response) => {
+  //const id = parseInt(request.params.id)
+  str_query = 'select a.product_id,a.product_name,a.product_category_name,a.sales sales_2020,b.sales sales_2019, a.sales - b.sales as difference from (select si.product_id,product_name, product_category_name, sum(sales_items) as sales from sales_insights si, product_master pm, product_categories pc where si.product_id = pm.product_id and pm.product_category_id = pc.product_category_id  group by si.product_id,product_name, product_category_name) a,(select sp.product_id,product_name, product_category_name, sum(sales_items) as sales from a_sales_by_product sp, product_master pm, product_categories pc where sp.product_id = pm.product_id and pm.product_category_id = pc.product_category_id and sp.year = 2019  group by sp.product_id,product_name, product_category_name) b where a.product_id = b.product_id'
+  pool.query(str_query,(error, results) => {
     if (error) {
       throw error
     }
-    response.status(200).send(`User deleted with ID: ${id}`)
+    response.status(200).json(results.rows)
   })
 }
+
+
+const getsalesbystore = (request, response) => {
+  //const id = parseInt(request.params.id)
+  str_query = 'select si.store_id, sm.Store_Name,sum(sales_items) sales from sales_insights si, store_master sm where si.store_id = sm.Store_ID group by si.store_id,sm.Store_Name order by si.store_id fetch first 6 rows only'
+  pool.query(str_query,(error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+
+
 
 module.exports = {
-  getUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
+  getSalesByStoreId,getSalesByCountries,getFSalesByProductCategories,getsalesreport,getsalesbystore
+ 
 }
